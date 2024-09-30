e1727d1905586154eca29e23fa8f2c7b617239e5
jeonck <ck.jeon@sk.com>
2024-10-01 08:48:35 +0900

    add: nav bar plain

---
+++ navigation-bar/nav-bar-pages/navigation-bar-blue.py
@@ -0,0 +1,60 @@
+import os
+
+import streamlit as st
+from streamlit_navigation_bar import st_navbar
+
+import pages as pg
+
+
+
+st.set_page_config(initial_sidebar_state="collapsed")
+
+
+
+pages = [ "Home", "Install", "User Guide", "API", "Examples", "Community", "GitHub"]
+parent_dir = os.path.dirname(os.path.abspath(__file__))
+logo_path = os.path.join(parent_dir, "cubes.svg")
+urls = {"GitHub": "https://github.com/gabrieltempass/streamlit-navigation-bar"}
+styles = {
+    "nav": {
+        "background-color": "royalblue",
+        "justify-content": "left",
+    },
+    "img": {
+        "padding-right": "14px",
+    },
+    "span": {
+        "color": "white",
+        "padding": "14px",
+    },
+    "active": {
+        "background-color": "white",
+        "color": "var(--text-color)",
+        "font-weight": "normal",
+        "padding": "14px",
+    }
+}
+options = {
+    "show_menu": False,
+    "show_sidebar": False,
+}
+
+page = st_navbar(
+    pages,
+    logo_path=logo_path,
+    urls=urls,
+    styles=styles,
+    options=options,
+)
+
+functions = {
+    "Home": pg.show_home,
+    "Install": pg.show_install,
+    "User Guide": pg.show_user_guide,
+    "API": pg.show_api,
+    "Examples": pg.show_examples,
+    "Community": pg.show_community,
+}
+go_to = functions.get(page)
+if go_to:
+    go_to()
---
+++ navigation-bar/navigation-bar-plain.py
@@ -0,0 +1,5 @@
+import streamlit as st
+from streamlit_navigation_bar import st_navbar
+
+page = st_navbar(["Home", "Documentation", "Examples", "Community", "About"])
+st.write(page)
